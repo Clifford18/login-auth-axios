26d88cc2ce269f13efa79900fba2e276df135aa6
Clifford18 <cliffordjohn.cj@gmail.com>
2022-05-05 17:00:32 +0300

    update App.js  file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import './App.css';
+import Login from './Login.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login/>
     </div>
   );
 }
