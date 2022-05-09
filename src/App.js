fe0c9eac7895da3f13512acc23f45bdf1c4eab6d
Clifford18 <cliffordjohn.cj@gmail.com>
2022-05-09 14:54:15 +0300

    update App.js file on refactoring

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Login from './Login.js';
+import LoginComponent from './components/LoginComponent.js';
 
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <LoginComponent/>
     </div>
   );
 }
