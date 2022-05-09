033ea9cb1a5278d645722756d2ea349ce1bf4845
Clifford18 <cliffordjohn.cj@gmail.com>
2022-05-09 14:52:31 +0300

    add and update LoginComponent.js

--- src/components/LoginComponent.js
+++ src/components/LoginComponent.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {useRef, useState, useEffect} from 'react';
-import './Login.css';
+import './LoginStyle.css';
 
-const Login = () => {
+const LoginComponent = () => {
 	const userRef = useRef();
 	const errRef = useRef();
 
@@ -80,4 +80,4 @@ const Login = () => {
 	);
 };
 
-export default Login;
+export default LoginComponent;
