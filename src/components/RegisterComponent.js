74b1ae1b139cd60ab59cb5e4945e3c8ee340fc6d
Clifford18 <cliffordjohn.cj@gmail.com>
2022-05-11 16:09:06 +0300

    update RegisterComponent.js on button

--- src/components/RegisterComponent.js
+++ src/components/RegisterComponent.js
@@ -117,9 +117,17 @@ const RegisterComponent = () => {
 					Must match the first password input field.
 				</p>
 
-
+				<button disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
 
 			</form>
+
+			<p>
+				Already registered?<br />
+				<span className="line">
+                            {/*put router link here*/}
+					<a href="#">Sign In</a>
+                        </span>
+			</p>
 
 		</section>
 	);
