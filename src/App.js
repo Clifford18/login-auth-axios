1bfd846c1399e2845fdfdb56c164c89afd222449
Clifford18 <cliffordjohn.cj@gmail.com>
2022-05-30 16:38:45 +0300

    update App.js file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import LoginComponent from './components/LoginComponent.js';
 import RegisterComponent from "./components/RegisterComponent";
+import ChangePasswordComponent from "./components/ChangePasswordComponent";
 
 function App() {
   return (
     <div className="App">
+        {/*<LoginComponent/>*/}
+        {/*<ChangePasswordComponent/>*/}
         <RegisterComponent/>
     </div>
   );
